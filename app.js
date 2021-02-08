6421385b570291d6da1b67f92d2aa8504085010c
Nazmul Ahsan <mnahsanofficial@gmail.com>
2021-02-08 23:51:18 +0600

    commit

--- app.js
+++ app.js
@@ -1,18 +1,74 @@
-const search=document.getElementById('search-button');
-search.addEventListener('click',function(){
-    foodDisplay();
-    
-})
+const searchFood = async () => {
+    const searchText = document.getElementById('search-food').value;
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?f=${searchText} `;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayFood(data.meals))
+        .catch(error => displayError('Something Went Wrong!! Please try again'))
+        
+}
 
-function foodDisplay(){
+const displayFood = foods => {
+    const foodContainer = document.getElementById('food-container');
+    foodContainer.innerHTML = '';
+    foods.forEach(meals => {
+        const foodDiv = document.createElement('div');
+        foodDiv.className = ' row-cols-md-6  mt-5 d-flex justify-content-around';
+        foodDiv.innerHTML = `
+        <div class="col">
+            <div class="card h-100 text-center">
+                <img onclick="getIngredient('${meals.strMeal}') "  src="${meals.strMealThumb}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">${meals.strMeal}</h5>
+                </div>
+            </div>
+        </div>
+       
+        `
+        foodContainer.appendChild(foodDiv);
 
-    const foodName=document.getElementById('search-food-name').innerText;
-    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?f=${foodName}`)
-    .then(resp=>resp.json())
-    .then(data=>{
-        foodName.innerText=data.strMeal;
-        console.log(foodName)
-    })
-    
+    });
 
+
+}
+
+const getIngredient=async(strMeal)=>{
+    const url=`https://www.themealdb.com/api/json/v1/1/search.php?s=${strMeal}`;
+    try {
+        const res = await fetch(url);
+        const data = await res.json();
+        displayIngredient(data.meals);
+
+    }
+    catch (error) {
+        displayError('Something Went Wrong!! Please try again');
+
+    }
+
+}
+const displayIngredient=ingredient=>{
+
+    ingredient.forEach(food => {
+        const ingredientContainer=document.getElementById('food-ingredient');
+        ingredientContainer.innerHTML = `
+        <div>
+            <p>${food.strIngredient1}</p>
+            <p>${food.strIngredient2}</p>
+            <p>${food.strIngredient3}</p>
+            <p>${food.strIngredient4}</p>
+            <p>${food.strIngredient5}</p>
+            <p>${food.strIngredient6}</p>
+            <p>${food.strIngredient7}</p>
+            <p>${food.strIngredient8}</p>
+            <p>${food.strIngredient9}</p>
+        </div>
+        
+        `
+        
+    });
+
+}
+const displayError=error=>{
+    const errorTag=document.getElementById('error-message')
+    errorTag.innerText=error;
 }
